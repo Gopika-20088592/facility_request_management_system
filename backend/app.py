529a217d4ab0fbc9e6f5f5727f48c54fd48b0e17
Gopika-20088592 <20088592@mydbs.ie>
2026-04-06 17:44:06 +0100

    I added a function to create a unique request ID for each request so that every request can be easily identified.

--- backend/app.py
+++ backend/app.py
@@ -10,3 +10,29 @@ def get_db_connection():
     connection = sqlite3.connect("facility_requests.db")
     connection.row_factory = sqlite3.Row
     return connection
+
+    def generate_request_id():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+
+    current_year = datetime.now().year
+    prefix = f"REQ-{current_year}-"
+
+    cursor.execute("""
+        SELECT request_id
+        FROM requests
+        WHERE request_id LIKE ?
+        ORDER BY id DESC
+        LIMIT 1
+    """, (f"{prefix}%",))
+
+    last_request = cursor.fetchone()
+    connection.close()
+
+    if last_request:
+        last_number = int(last_request["request_id"].split("-")[-1])
+        new_number = last_number + 1
+    else:
+        new_number = 1
+
+    return f"REQ-{current_year}-{str(new_number).zfill(3)}"
