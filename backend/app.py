50c8c46d13f32952156a10ac71a9ed342437d608
Gopika-20088592 <20088592@mydbs.ie>
2026-04-06 16:45:23 +0100

    Created Flask app, enable CORS, and added SQLite database connection

--- backend/app.py
+++ backend/app.py
@@ -2,3 +2,11 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import sqlite3
 from datetime import datetime
+
+app = Flask(__name__)
+CORS(app)
+
+def get_db_connection():
+    connection = sqlite3.connect("facility_requests.db")
+    connection.row_factory = sqlite3.Row
+    return connection
